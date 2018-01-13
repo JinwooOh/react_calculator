68435dbb151ed8ee7a12d5f5d7be963f0e81d2ea
Jinwoo <ozi0414@gmail.com>
2018-01-13 13:34:57 -0600

    added all files

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+const config = {
+  //entry
+  entry: path.resolve('src/index.js'),
+  //where output?
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  //load multiple loaders
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader'
+      },{
+        test: /\.css$/,
+        loader: 'style-loader'
+      },{
+        test: /\.css$/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, ""),
+    compress: true,
+    port: 9000
+  }
+
+
+};
+
+module.exports = config;
